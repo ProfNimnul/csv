5cae5b378d44f14e3ccda76e014be31f53b38089
Stas <C:\Users\User\AppData\Roaming\The Bat!>
2017-07-16 21:48:58 +0300

    Надо изменить функцию анализа нестандартных строк

--- csv_validator.py
+++ csv_validator.py
@@ -15,24 +15,35 @@ def get_csv_file ():
 
 
 # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-current_category = 0
+#current_category = 0
+checked_categories = []  # перелік категорій, які ми вже обробили
+checked_categories_total = []  # перелік категорій, які ми вже обробили
+
+nonstandard_categories = [  "І.ТОВАРИ" ,
+                    "ІІ.РОБОТИ" ,
+                    "ІІІ.ПОСЛУГИ",
+                    "Усього за розділом І (ТОВАРИ)" ,
+                    "Усього за розділом ІІ (РОБОТИ)" ,
+                    "Усього за розділом ІІІ (Послуги)",
+                    "Разом (розділ І+розділ ІІ+розділ ІІІ)"
+                    "КВЕД"
+
+                   ]
 # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 # Функція зчитує СSV-файл
 def read_from_csv ( fname ):
     global current_category
     csv.register_dialect ( "edata" , delimiter=';' )  # Символ - розділювач токенів
-    list_categories = [ "І.ТОВАРИ" , "ІІ.РОБОТИ" , "ІІІ.ПОСЛУГИ" ]
+
+
     try:
         with open ( fname , 'rt' , encoding='Windows-1251' ) as csv_file:
             row_count = 1
             for row in csv.DictReader ( csv_file , dialect='edata' ):
                 row_count += 1
 
-                if row[ 'Назва контрагента' ] in list_categories:  # обробляємо категорії
-                    checked_categories.append(row[ 'Назва контрагента' ])
-                    current_category+=1
+                if val_nestandartni_stroki(row,row_count): #перевіряємо-чи нестандартна це строка
                     continue
-
 
                 for col in row:
                     # print (row[col])
@@ -47,24 +58,60 @@ def read_from_csv ( fname ):
 
 # =================== Функції - валідатори
 
+def val_nestandartni_stroki(row, row_count):
+    global current_category
+
+    n=row['Назва контрагента']
+    if len(n) >0 and (n in nonstandard_categories):
+    # обробляємо випадок "І.ТОВАРИ" , "ІІ.РОБОТИ" , "ІІІ.ПОСЛУГИ"
+        checked_categories.append ( n )
+        current_category = nonstandard_categories.index(n)+1
+
+        return True
+    else:
+        return False
+
+    n = row[ "№з/п" ]
+    if  len(n) >0 and (n in nonstandard_categories):
+        # обробляємо випадок "Усього за розділом І (ТОВАРИ)" , "Усього за розділом ІІ (РОБОТИ)" ,
+        # "Усього за розділом ІІІ (Послуги)" - стовпчик row[ '№з/п' ]
+
+        checked_categories.append ( row[ "№з/п" ] )
+        if str(n).find("Усього за розділом")>0:
+            if len(row['Обсяг платежів, грн.']) == 0:
+                print ( 'Строка № %i : Відсутній підсумок за розділом %i!' % (row_count,n) )
+            return True
+
+
+        elif str(n).find('Разом') >0:
+            # обробляємо випадок "Разом (розділ І+розділ ІІ+розділ ІІІ)" ,
+            if len(row['Обсяг платежів, грн.']) == 0:
+                print ( 'Строка № %i : Відсутній загальний підсумок за усіма розділами!' % (row_count) )
+            return True
+        elif str(n).find('КВЕД') >0:
+            if len(row['Обсяг платежів, грн.']) == 0:
+                print ( 'Строка № %i : Відсутній код КВЕД!' % (row_count) )
+            return True
+    else:
+        return False
+
 
 
 def val_nomer_za_poryadkom ( val , row_count=0 ):
     global current_category
-    l=len(val)
-    if l == 0:
+    l = len ( val )
+    cat = int ( val )
+    if current_category != cat:
+        print ( 'Строка № %i : Категорія %i не співпадає з поточною - %i!' % (row_count , cat , current_category) )
+
+    elif l == 0:
         print ( 'Строка № %i : не вказано категорію (1,2 чи 3)!' % row_count )
-
-    elif current_category != int(val):
-        print ( 'Строка № %i : Категорія  не співпадає з поточною - !' % row_count)
     return
-
-
 
 
 # перевіряємо дату договору - поле "Від"
 def val_vid ( val , row_count=0 ):
-    l=len(val)
+    l = len ( val )
     if l == 0:
         print ( 'Строка № %i : Відсутня дата договору!' % row_count )
     elif 0 < l < 10:
@@ -94,8 +141,6 @@ def val_dogovir_no ( val , row_count=0 ):
 def val_nazva_kontragenta ( val , row_count=0 ):
     l = len ( val )
 
-
-
     if l == 0:
         print ( 'Строка № %i : Відсутня назва контрагента' % row_count )
     elif (l > 120):
@@ -106,7 +151,7 @@ def val_nazva_kontragenta ( val , row_count=0 ):
 
 def val_predmet_dogovoru ( val , row_count=0 ):
     l = len ( val )
-    s=str(val)
+    s = str ( val )
     if l == 0:
         print ( 'Строка № %i : Не заповнено предмет договору' % row_count )
     elif (l > 256):
@@ -117,7 +162,7 @@ def val_predmet_dogovoru ( val , row_count=0 ):
 
 def val_kod_cpv_rozdil ( val , row_count=0 ):
     l = len ( val )
-    s=str(val)
+    s = str ( val )
     if l == 0:
         print ( 'Строка № %i : Відсутній код СPV' % row_count )
 
@@ -153,9 +198,6 @@ validators = {
     'Обсяг платежів, грн.': val_obsyag_plategiv_grn
 }
 
-
-checked_categories =[] # перелік категорій, які ми вже обробили
-
 fname = get_csv_file ( )
 read_from_csv ( fname )
-print(checked_categories)
+print ( checked_categories )
